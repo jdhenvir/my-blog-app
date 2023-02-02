f569e27cd0a16e8363648b2e48287638ed2ec47f
James Dhenvir <118252954+jdhenvir@users.noreply.github.com>
2023-02-02 23:51:37 +0800

    pp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   MDBContainer,
   MDBNavbar,
@@ -10,54 +10,48 @@ import {
   MDBNavbarItem,
   MDBNavbarLink,
   MDBCollapse,
-} from 'mdb-react-ui-kit';
+} from "mdb-react-ui-kit";
 
 export default function Navbar() {
   const [showBasic, setShowBasic] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' dark bgColor='black'>
+    <MDBNavbar expand="lg" dark bgColor="black">
       <MDBContainer>
-        <MDBNavbarBrand href='/'>DhenFolio</MDBNavbarBrand>
+        <MDBNavbarBrand href="/">DhenFolio</MDBNavbarBrand>
 
         <MDBNavbarToggler
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
           onClick={() => setShowBasic(!showBasic)}
         >
-          <MDBIcon icon='bars' fas />
+          <MDBIcon icon="bars" fas />
         </MDBNavbarToggler>
 
         <MDBCollapse navbar show={showBasic}>
-
-          <MDBNavbarNav right fullWidth={false} className='mb-2 mb-lg-0'>
+          <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
             <Link to="/">
               <MDBNavbarItem>
-                <MDBNavbarLink>
-                    Home
-                </MDBNavbarLink>
+                <MDBNavbarLink>Home</MDBNavbarLink>
               </MDBNavbarItem>
             </Link>
             <Link to="/profile">
-            <MDBNavbarItem>
-              <MDBNavbarLink>
-                Profile
-              </MDBNavbarLink>
-            </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink>Profile</MDBNavbarLink>
+              </MDBNavbarItem>
             </Link>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='/projects'>
-                Projects
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='blogpost'>
-                Blog
-              </MDBNavbarLink>
-            </MDBNavbarItem>
+            <Link to="/projects">
+              <MDBNavbarItem>
+                <MDBNavbarLink>Projects</MDBNavbarLink>
+              </MDBNavbarItem>
+            </Link>
+            <Link to="/blogpost">
+              <MDBNavbarItem>
+                <MDBNavbarLink>Blog</MDBNavbarLink>
+              </MDBNavbarItem>
+            </Link>
           </MDBNavbarNav>
-  
         </MDBCollapse>
       </MDBContainer>
     </MDBNavbar>
